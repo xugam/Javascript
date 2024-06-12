e8a5cb3080b64879b9a93dcd23f0c906c556ceed
Sugam Raj Prasai <146755084+xugam@users.noreply.github.com>
2024-06-12 10:30:53 +0545

    First day of JS

---
+++ Javascript/First.js
@@ -0,0 +1,13 @@
+
+console.log("Hello From JS");
+let q = 5;
+var r = 5;
+let nm = "Sugam";
+const name= document.getElementById("name");
+console.log(name);
+
+function changecolor(Name){
+    document.body.style.backgroundColor=Name;
+    name.innerHTML = Name;
+    console.log(name);
+}
